3ddb5de62eaf2738ac5c805f98842c81cf6d3e22
LordUche <uchenna.a.i@gmail.com>
2021-03-01 19:10:21 +0100

    about page

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+const About = () => {
+  return (
+    <div>
+      <Head>
+        <title>David's App | About</title>
+        <meta name="description" content="David's about page" />
+        <meta name="keywords" content="David, about" />
+      </Head>
+
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita autem excepturi sit, unde tenetur commodi
+        similique nisi architecto nemo ut odit voluptatibus harum itaque, rerum dolores reiciendis quod quasi maiores!
+      </p>
+    </div>
+  )
+}
+
+export default About
